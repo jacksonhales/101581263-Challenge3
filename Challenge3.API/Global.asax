07d62bc653f5b5107554ab4259fcaa8dd9f19465
Jackson Hales <me@jacksonhales.com.au>
2018-09-21 10:16:55 +1000

    Created API project

---
+++ Challenge3.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Challenge3.API.MvcApplication" Language="C#" %>
---
+++ Challenge3.API/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Challenge3.API.Startup))]
+namespace Challenge3.API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
